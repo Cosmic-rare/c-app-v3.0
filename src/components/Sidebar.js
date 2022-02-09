1d6d2430111a47ce1f7a7873f7d71caa5ccf97d1
Cosmic-rare <ysolakey1116@gmail.com>
2022-02-09 18:22:53 +0900

    Use map

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,40 +6,43 @@ const Sidebar = (props) => {
     changeIsOpen
   } = props
 
-  return(
+  const channels = [
+    "雑談1",
+    "雑談2",
+    "音ゲーマー",
+    "アンケート",
+    "プロセカ",
+    "勉強の相談"
+  ]
+
+  const directMessages = [
+    "谷さん1号",
+    "谷さん2号",
+    "谷さん3号"
+  ]
+
+  return (
     <div className={isOpen ? "sidebar open" : "sidebar close"}>
       <button className="close-toggle" onClick={changeIsOpen}>X</button>
 
       <Toggle message={"Channles"}>
-        <li className="list-item">
-          雑談1
-        </li>
-        <li className="list-item">
-          雑談2
-        </li>
-        <li className="list-item">
-          音ゲーマー
-        </li>
-        <li className="list-item">
-          アンケート
-        </li>
-        <li className="list-item">
-          プロセカ
-        </li>
-        <li className="list-item">
-          勉強の相談
-        </li>
+        {channels.map((value) => {
+          return (
+            <li className="list-item">
+              {value}
+            </li>
+          )
+        })}
       </Toggle>
-      <Toggle message={"DirectMessage"}>
-        <li className="list-item">
-          谷さん１号
-        </li>
-        <li className="list-item">
-          谷さん２号
-        </li>
-        <li className="list-item">
-          谷さん３号
-        </li>
+
+      <Toggle message={"DirectMessages"}>
+        {directMessages.map((value) => {
+          return (
+            <li className="list-item">
+              {value}
+            </li>
+          )
+        })}
       </Toggle>
 
     </div>
