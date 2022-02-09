4b5fdc929a517e79a9bf7f70f61a7195f34bd271
Cosmic-rare <ysolakey1116@gmail.com>
2022-02-09 18:18:03 +0900

    Add sample-data

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,14 +9,39 @@ const Sidebar = (props) => {
   return(
     <div className={isOpen ? "sidebar open" : "sidebar close"}>
       <button className="close-toggle" onClick={changeIsOpen}>X</button>
+
       <Toggle message={"Channles"}>
         <li className="list-item">
           雑談1
         </li>
         <li className="list-item">
-          雑談2fdffdfdfdfddfdfdfdfddfddfdfdfdfdfdffddffddfdfdfdfdfsdfsdfsdfssss
+          雑談2
+        </li>
+        <li className="list-item">
+          音ゲーマー
+        </li>
+        <li className="list-item">
+          アンケート
+        </li>
+        <li className="list-item">
+          プロセカ
+        </li>
+        <li className="list-item">
+          勉強の相談
         </li>
       </Toggle>
+      <Toggle message={"DirectMessage"}>
+        <li className="list-item">
+          谷さん１号
+        </li>
+        <li className="list-item">
+          谷さん２号
+        </li>
+        <li className="list-item">
+          谷さん３号
+        </li>
+      </Toggle>
+
     </div>
   )
 }
